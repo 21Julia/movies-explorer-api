227667e6585ea3e0ddbe2fc2142550144e47f018
Julia Kurganova <juliakurganova21@yandex.ru>
2023-08-22 15:50:29 +0300

    refactor: добавить ветку

---
+++ app.js
@@ -0,0 +1,13 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const helmet = require('helmet');
+// const rateLimit = require('express-rate-limit');
+// const bodyParser = require('body-parser');
+// const cookieParser = require('cookie-parser');
+// const { errors } = require('celebrate');
+
+// const routes = require('./routes/index');
+
+// const { PORT = 3000, SERVER_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+// const app = express();
